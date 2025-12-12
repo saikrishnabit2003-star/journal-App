f5e054ba7d4cc2d7501d67bdf31992b6b7aa4e20
saikrishnabit2003-star <saikrishnabit2003@gmail.com>
2025-12-12 10:31:46 +0530

    change url

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,8 +14,8 @@ function Header({ setIsLoggedIn }) {
     <div>
       <div className="header">
         <div className="top">
-         <a href="/homepage"> <img src={Logo} alt="Logo" /></a>
-         <a href="/homepage"> <img src={text} alt="Logo" /></a>
+         <a href="https://journal-suggestion-backend-fastapi.vercel.app/homepage"> <img src={Logo} alt="Logo" /></a>
+         <a href="https://journal-suggestion-backend-fastapi.vercel.app/homepage"> <img src={text} alt="Logo" /></a>
           <button id="logbtn" onClick={logout}>Log out</button>
         </div>
       </div>
